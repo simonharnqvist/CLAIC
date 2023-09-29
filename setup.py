f97c5bc543254e506b888800cfcc09cb004cd227
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-09-29 15:31:18 +0100

    removed long description

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
+import pypandoc
 
 setup(
     name='iclik',
     version='0.06-alpha',
     packages=['iclik'],
-    description="Information criteria for composite likelihood models"
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    description="Information criteria for composite likelihood models",
 )
