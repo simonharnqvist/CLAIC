99b5ed8db7c29bd7838abdcf6079c6d64c2a5f88
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-09-29 15:40:42 +0100

    attempting with RST as long_description

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(
     version='0.06-alpha',
     packages=['iclik'],
     description="Information criteria for composite likelihood models",
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
 )
