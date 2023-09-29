0391047ad7c55d7c7f4587f03776b4e227ac5c75
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-09-29 13:51:20 +0100

    new approach to open README in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from setuptools import setup
-from pathlib import Path
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='iclik',
     version='0.06-alpha',
     packages=['iclik'],
-    long_description=long_description,
-    long_description_content_type="text/markdown"
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
 )
