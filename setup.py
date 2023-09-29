b66efd193be2b5f532fd13ab0725802bc600df25
simonharnqvist <45334810+simonharnqvist@users.noreply.github.com>
2023-09-29 15:19:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='iclik',
     version='0.06-alpha',
     packages=['iclik'],
+    description="Information criteria for composite likelihood models"
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
