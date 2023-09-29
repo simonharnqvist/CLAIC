67d777fb7270a4275aa92b41442376f89f51de13
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-09-29 13:46:01 +0100

    specified README in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='iclik',
     version='0.06-alpha',
     packages=['iclik'],
-    long_description="README.md"
+    long_description=long_description,
+    long_description_content_type="text/markdown"
 )
